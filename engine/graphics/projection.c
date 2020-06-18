c26927a1d92981fa77eed897fb47eb18964a1345
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-06-18 09:03:35 +0200

    Added projection source

---
+++ engine/graphics/projection.c
@@ -0,0 +1,40 @@
+#include "projection.h"
+
+void    PRJ_initProjection  (float  aspRat) {
+    fov = 45.0f;
+    aspectRatio = aspRat;
+    qsrPerspectiveMat4f(*projPerspectiveMatrix, FOV_3D, aspRat, NEAREST, FAREST);
+}
+
+void    PRJ_setOrthogonal   () {
+    mode = 0;
+}
+
+void    PRJ_setPerspective  () {
+    mode = 1;
+}
+
+void    PRJ_zoom            (float  yoffset) {
+    if  (mode == 0) {
+        fov -= yoffset;
+        if      (fov < 1.0f)
+            fov = 1.0f;
+        else if     (fov > 45.0f)
+            fov = 45.0f;
+    }
+    else {
+        printf("[DEBUG] Tried to Change FOV in 3D Mode\n");
+    }
+}
+
+mat4    PRJ_returnProjection    () {
+    if      (mode == 0) {
+        /* Return and recalculate orthogonal matrix. */
+        qsrOrthogonalMat4f(*projOrthogonalMatrix, fov, aspectRatio, NEAREST, FAREST);
+        return projOrthogonalMatrix;
+    }
+    else if     (mode == 1) {   
+        /* Return perspective matrix. */
+        return projPerspectiveMatrix;
+    }
+}
--- engine/graphics/projection.h
+++ engine/graphics/projection.h
@@ -5,6 +5,10 @@
 #ifndef THE_FOURTH_AGE_PROJECTION_H
 #define THE_FOURTH_AGE_PROJECTION_H
 
+#define NEAREST 0.0f
+#define FAREST  1.0f
+#define FOV_3D  90.0f
+
 /* Includes the math library to be able to make vector and matrix calculations */
 #include <maths.h>
 
@@ -16,24 +20,24 @@ mat4    projPerspectiveMatrix;
 mat4    projOrthogonalMatrix;
 
 /* Current FOV to be changed by zooming */
-int     fov;
+float   fov;
+
+/* To save the aspect ratio of the resolution */
+float   aspectRatio;
 
 /* Initializes variables and matrices to default; to be called at start of program */
-void    PROJ_initProjection     ();
+void    PRJ_initProjection      (float  aspRat);
 
 /* Changes mode to 0 */
-void    PROJ_setOrthogonal      ();
+void    PRJ_setOrthogonal       ();
 
 /* Changes mode to 1 */
-void    PROJ_setPerspective     ();
+void    PRJ_setPerspective      ();
 
-/* Changes FOV on zoom in, only if mode = 0 */
-void    PROJ_zoomIn             ();
-
-/* Changes FOV on zoom out */
-void    PROJ_zoomOut            ();
+/* Changes FOV on zoom, only if mode = 0 */
+void    PRJ_zoom                (float  yoffset);
 
 /* Returns the current projection matrix only if mode = 0 */
-mat4    returnProjection        ();
+mat4    PRJ_returnProjection    ();
 
 #endif //THE_FOURTH_AGE_PROJECTION_H
