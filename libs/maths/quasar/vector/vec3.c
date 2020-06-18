3c05bc0247fdedd63a04cf0dc76813077838ce1b
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-06-18 20:54:21 +0200

    Added more vector functions

--- libs/maths/quasar/vector/vec3.c
+++ libs/maths/quasar/vector/vec3.c
@@ -12,22 +12,18 @@ float qsrGetVec3fLength(QsrVec3f* vec) {
 	return sqrtf((vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z));
 }
 
-QsrVec3f qsrNormalizeVec3f(QsrVec3f* vec) {
+QsrVec3f qsrNormalizeVec3f(QsrVec3f vec) {
 	
 	QsrVec3f output;
 	
-	float length = qsrGetVec3fLength(vec);
+	float length = qsrGetVec3fLength(&vec);
 	
-	output.x = vec->x  / length;
-	output.y = vec->y  / length;
-	output.z = vec->z  / length;
+	output.x = vec.x  / length;
+	output.y = vec.y  / length;
+	output.z = vec.z  / length;
 	
 	return output;
 }
-
-
-
-
 
 void qsrInitVec3i(QsrVec3i* vec) {
 	
@@ -41,22 +37,18 @@ float qsrGetVec3iLength(QsrVec3i* vec) {
 	return sqrtf((vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z));
 }
 
-QsrVec3i qsrNormalizeVec3i(QsrVec3i* vec) {
+QsrVec3i qsrNormalizeVec3i(QsrVec3i vec) {
 	
 	QsrVec3i output;
 	
-	float length = qsrGetVec3iLength(vec);
+	float length = qsrGetVec3iLength(&vec);
 	
-	output.x = vec->x  / length;
-	output.y = vec->y  / length;
-	output.z = vec->z  / length;
+	output.x = vec.x  / length;
+	output.y = vec.y  / length;
+	output.z = vec.z  / length;
 	
 	return output;
 }
-
-
-
-
 
 void qsrInitVec3u(QsrVec3u* vec) {
 	
@@ -70,16 +62,49 @@ float qsrGetVec3uLength(QsrVec3u* vec) {
 	return sqrtf((vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z));
 }
 
-QsrVec3u qsrNormalizeVec3u(QsrVec3u* vec) {
+QsrVec3u qsrNormalizeVec3u(QsrVec3u vec) {
 	
 	QsrVec3u output;
 	
-	float length = qsrGetVec3uLength(vec);
+	float length = qsrGetVec3uLength(&vec);
 	
-	output.x = vec->x  / length;
-	output.y = vec->y  / length;
-	output.z = vec->z  / length;
+	output.x = vec.x  / length;
+	output.y = vec.y  / length;
+	output.z = vec.z  / length;
 	
 	return output;
 }
+
+QsrVec3f qsrAddVec3f(QsrVec3f vec1, QsrVec3f vec2) {
+    QsrVec3f temp;
+    temp.x  =   vec1.x + vec2.x;
+    temp.y  =   vec1.y + vec2.y;
+    temp.z  =   vec1.z + vec2.z
+    return temp;
+}
+
+QsrVec3f qsrSubVec3f(QsrVec3f vec1, QsrVec3f vec2) {
+    QsrVec3f temp;
+    temp.x  =   vec1.x - vec2.x;
+    temp.y  =   vec1.y - vec2.y;
+    temp.z  =   vec1.z - vec2.z;
+    return temp;
+}
+
+QsrVec3f qsrCrossVec3f(QsrVec3f vec1, QsrVec3f vec2) {
+    QsrVec3f temp;
+    temp.x  =   vec1.y * vec2.z - vec1.z * vec2.y;
+    temp.y  =   vec1.z * vec2.x - vec1.x * vec2.z;
+    temp.z  =   vec1.x * vec2.y - vec2.y * vec1.x;
+    return temp;
+}
+
+QsrVec3f qsrScaleVec3f(QsrVec3f vec, float scalar) {
+    QsrVec3f temp;
+    temp.x  =   vec.x * scalar;
+    temp.y  =   vec.y * scalar;
+    temp.z  =   vec.z * scalar;
+    return temp;
+}
+
 
--- libs/maths/quasar/vector/vec3.h
+++ libs/maths/quasar/vector/vec3.h
@@ -31,19 +31,23 @@ typedef struct {
 void qsrInitVec3f(QsrVec3f* vec);
 
 float qsrGetVec3fLength(QsrVec3f* vec);
-QsrVec3f qsrNormalizeVec3f(QsrVec3f* vec);
-
+QsrVec3f qsrNormalizeVec3f(QsrVec3f vec);
 
 
 void qsrInitVec3i(QsrVec3i* vec);
 
 float qsrGetVec3iLength(QsrVec3i* vec);
-QsrVec3i qsrNormalizeVec3i(QsrVec3i* vec);
+QsrVec3i qsrNormalizeVec3i(QsrVec3i vec);
 
 
 
 void qsrInitVec3u(QsrVec3u* vec);
 
 float qsrGetVec3uLength(QsrVec3u* vec);
-QsrVec3u qsrNormalizeVec3u(QsrVec3u* vec);
+QsrVec3u qsrNormalizeVec3u(QsrVec3u vec);
 
+QsrVec3f qsrAddVec3f(QsrVec3f vec1, QsrVec3f vec2);
+QsrVec3f qsrSubVec3f(QsrVec3f vec1, QsrVec3f vec2);
+
+QsrVec3f qsrCrossVec3f(QsrVec3f vec1, QsrVec3f vec2);
+QsrVec3f qsrScaleVec3f(QsrVec3f vec, float scalar);
