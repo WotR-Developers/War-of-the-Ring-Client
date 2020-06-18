f8d09a0390821a750ce53da11e27074e8ad424e3
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-06-18 10:07:04 +0200

    Added camera source

--- engine/graphics/camera2d.c
+++ engine/graphics/camera2d.c
@@ -1,33 +1,52 @@
 #include "camera.h"
 
 void    CA2_initCam     () {
-
+    cameraPosition2d =   {0.0f, 1.0f, 0.0f};
+    cameraFront2d    =   {0.0f, 0.0f, -1.0f};
+    cameraUp2d       =   {0.0f, 1.0f, 0.0f};
+    worldUp2d        =   {0.0f, 1.0f, 0.0f};
+    movementSpeed2d  =   0.05f;
+    mouseSensitivity2D   =   0.1f;
 }
 
 void    CA2_moveForward () {
-
+    cameraPosition2d[0] += cameraFront2d[0] * movementSpeed2d;
+    cameraPosition2d[2] += cameraFront2d[2] * movementSpeed2d;
 }
 
 void    CA2_moveBack    () {
-
+    cameraPosition2d[0] -= cameraFront2d[0] * movementSpeed2d;
+    cameraPosition2d[2] -= cameraFront2d[2] * movementSpeed2d;
 }
 
 void    CA2_strafeRight () {
-
+    cameraPosition2d[0] += cameraRight2d[0] * movementSpeed2d;
+    cameraPosition2d[2] += cameraRight2d[2] * movementSpeed2d;
 }
 
 void    CA2_strafeLeft  () {
-
+    cameraPosition2d[0] -= cameraRight2d[0] * movementSpeed2d;
+    cameraPosition2d[2] -= cameraRight2d[2] * movementSpeed2d;
 }
 
 void    CA2_rotateRight () {
-
+    yaw2d += 1.0f;
 }
 
 void    CA2_rotateLeft  (){
-
+    yaw2d -= 1.0f;
 }
 
 mat4    CA2_getViewMatrix   () {
-
+    cameraFront2d[0]    =   cos(radians(yaw2d)) * cos(radians(pitch2d));
+    cameraFront2d[1]    =   sin(radians(pitch2d));
+    cameraFront2d[2]    =   sin(radians(yaw2d)) * cos(radians(pitch2d));
+    normalize(cameraFront2d);
+    cameraRight2d       =   normalize(cross(cameraFront2d, worldUp2d));
+    normalize(cameraRight2d);
+    cameraUp2d          =   normalize(cross(cameraRight2d, cameraFront2d));
+    normalize(cameraUp2d);
+    vec3 tempVector;
+    tempVector      =   add(cameraPosition2d, cameraFront2d);
+    return  lookAt(cameraPosition2d, tempVector, cameraUp2d);
 }
--- engine/graphics/camera3d.c
+++ engine/graphics/camera3d.c
@@ -1,33 +1,56 @@
 #include "camera.h"
 
 void    CA3_initCam     () {
-
+    cameraPosition  =   {0.0f, 1.0f, 0.0f};
+    cameraFront     =   {0.0f, 0.0f, -1.0f};
+    cameraUp        =   {0.0f, 1.0f, 0.0f};
+    worldUp         =   {0.0f, 1.0f, 0.0f};
+    movementSpeed   =   0.05f;
+    mouseSensitivity   =   0.1f;
 }
 
 void    CA3_moveForward () {
-
+    cameraFront     =   scale(cameraFront, movementSpeed);
+    cameraPosition  =   add(cameraPosition, cameraFront);
 }
 
 void    CA3_moveBack    () {
-
+    cameraFront     =   scale(cameraFront, movementSpeed);
+    cameraPosition  =   sub(cameraPosition, cameraFront);
 }
 
 void    CA3_strafeRight () {
-
+    vec3 temp;
+    temp = normalize(cross(cameraFront, cameraUp));
+    temp = scale(temp, movementSpeed);
+    cameraPosition = add(cameraPosition, temp);
 }
 
 void    CA3_strafeLeft  () {
-
+    vec3 temp;
+    temp = normalize(cross(cameraFront, cameraUp));
+    temp = scale(temp, movementSpeed);
+    cameraPosition = sub(cameraPosition, temp);
 }
 
 void    CA3_rotateRight () {
-
+    yaw += 1.0f;
 }
 
 void    CA3_rotateLeft  (){
-
+    yaw -= 1.0f;
 }
 
 mat4    CA3_getViewMatrix   () {
-
+    cameraFront[0]      =   cos(radians(yaw)) * cos(radians(pitch));
+    cameraFront[1]      =   sin(radians(pitch));
+    cameraFront[2]      =   sin(radians(yaw)) * cos(radians(pitch));
+    normalize(cameraFront);
+    cameraRight         =   normalize(cross(cameraFront, worldUp));
+    normalize(cameraRight);
+    cameraUp            =   normalize(cross(cameraRight, cameraFront));
+    normalize(cameraUp);
+    vec3 tempVector;
+    tempVector      =   add(cameraPosition, cameraFront);
+    return  lookAt(cameraPosition, tempVector, cameraUp);
 }
